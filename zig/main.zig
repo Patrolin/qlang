6291f2dbd7de25e979632ee7436218aff7a19447
Patrolin <patrik@balas.cz>
2023-03-06 15:39:19 +0100

    ask_user

--- zig/main.zig
+++ zig/main.zig
@@ -1,9 +1,22 @@
 // zig build-obj -femit-llvm-ir .\zig\main.zig
 const std = @import("std");
-const Allocator = std.mem.Allocator;
-const allocator = std.heap.page_allocator;
 const dprint = std.debug.print;
 
 pub fn main() !void {
-    dprint("Hello world\n", .{});
+    var b = try ask_user();
+    // 1) auto check for zero - slow
+    // 2) Itanium exception handler?
+    // 3) SEH/longjump - very slow
+    var x = @as(i16, -32768) * @as(i16, b); // panic: integer overflow
+    var y = @divTrunc(100, b); // panic: divide by zero
+    dprint("Hello world {}, {}\n", .{x, y});
+}
+
+pub fn ask_user() !i16 {
+    const stdin = std.io.getStdIn().reader();
+    const stdout = std.io.getStdOut().writer();
+    try stdout.print("Number: ", .{});
+    var buf: [20]u8 = undefined;
+    const n = try stdin.read(&buf);
+    return std.fmt.parseInt(i16, buf[0..n-2], 10);
 }
